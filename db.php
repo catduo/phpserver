db54131bb6545c3b834cbacd03cfb64c38c75efe
David Geisert <dg@catduo.com>
2014-12-19 13:21:47 -0800

    checking passwordd hasning

--- db.php
+++ db.php
@@ -7,6 +7,7 @@
         }
 		
 		public function ping($test, $test2){
+			php_info();
 			$stmt = mysqli_prepare($this->conn, "SELECT PasswordHash FROM games.player_accounts WHERE Username= ?");
 			mysqli_stmt_bind_param($stmt, 's', $test2);
 			mysqli_stmt_execute($stmt);
