4521008a38406f43ed5c033c715863cb7f74441e
David Geisert <dg@catduo.com>
2014-12-22 14:15:24 -0800

    testing full php async system

--- setInvite.php
+++ setInvite.php
@@ -1,11 +1,11 @@
 <?php
     require_once("connect.php");
 	if(isset($_POST['email']) && isset($_POST['gameID'])){
-		echo "{'msg':'";
+		echo "{'msg':";
 		
 		$data = $db->setInvite($_POST['email'], $_POST['gameID']);
 		echo $data[0]['JoviosID'];
-		echo "','email':'";
+		echo ",'email':'";
 		echo $_POST['email'];
 		
 		echo "'}";
