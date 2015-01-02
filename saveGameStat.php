106df145bd9ca1a22c8aec5ff3d579e24db6dbb4
David Geisert <dg@catduo.com>
2015-01-02 14:26:04 -0800

    getting post parameters to work right for saveGameStat

--- saveGameStat.php
+++ saveGameStat.php
@@ -2,7 +2,7 @@
     require_once("connect.php");
 	if(isset($_POST['gameName']) && isset($_POST['deviceID']) && isset($_POST['species'])){
 		echo "{'msg':'";
-		echo $db->saveGame($_POST['deviceID'], $_POST['gameName'], $_POST['domain'], $_POST['kingdom'], $_POST['phylum'], $_POST['order'], $_POST['statClass'], $_POST['family'], $_POST['genus'], $_POST['species']);
+		echo $db->saveGameStat($_POST['deviceID'], $_POST['gameName'], $_POST['domain'], $_POST['kingdom'], $_POST['phylum'], $_POST['order'], $_POST['statClass'], $_POST['family'], $_POST['genus'], $_POST['species']);
 		echo "'}";
 	}
 	else{
