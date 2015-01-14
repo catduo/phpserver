015658d8fbc84237bc92a3121ab81d25d93ea4c8
David Geisert <dg@catduo.com>
2015-01-14 14:43:09 -0800

    setting up load game for acting as server

--- loadGame.php
+++ loadGame.php
@@ -1,6 +1,15 @@
 <?php
     require_once("connect.php");
 	if(isset($_POST['joviodID']) && isset($_POST['deviceType']) && isset($_POST['deviceInfo'])){
-		$data = $db->saveGame($_POST['joviodID'], $_POST['deviceType'], $_POST['deviceInfo']);
+    	echo "{'msg':[";
+		echo"{";
+		echo "'GameID':";
+		echo $game['GameID'];
+		echo ",'GameState':";
+		
+		$data = $db->loadGame($_POST['gameID']);
 		echo $data[0]['gameState'];
+		
+		echo"}";
+		echo "]}";
 	}
